5419eba0e3655d625a3d8b0107ce5cc7996e9ebd
Maddie Higgins <maddiehiggins9@gmail.com>
2020-10-31 15:24:55 -0500

    move app contents to game component

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 export { default as StartScreen } from './StartScreen'
-export { default as EndScreen } from './EndScreen'
-// export { default as Game } from './Game'
+export { default as Game } from './Game'
 export { default as Display } from './Display'
 export { default as Question } from './Question'
 export { default as PotentialAnswer } from './PotentialAnswer'
